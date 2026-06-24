bdd238805da4e84a3ac4e36814a6f52846a9ba4d
Timofei Rusanov <timkingteam@gmail.com>
2026-06-24 14:13:16 +0200

    b

--- vec_add.cu
+++ vec_add.cu
@@ -11,6 +11,7 @@ __global__ void vecAdd(const float *a, const float *b, float *c, int n) {
 }
 
 int main() {
+    pritnf("HIIIII\n");
     const int N = 1024;
     const size_t bytes = N * sizeof(float);
 
